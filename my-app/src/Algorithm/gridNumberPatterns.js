6e4ce804ef0d280e8a374e5cd90ca8936c1630b7
MO7A <70697952>
2023-07-01 15:10:12 -0700

    project is done

--- my-app/src/Algorithm/gridNumberPatterns.js
+++ my-app/src/Algorithm/gridNumberPatterns.js
@@ -1,17 +1,22 @@
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const gridNumbersPattern = (rows, cols) => {
+const gridNumbersPatternHelper = (rows, cols) => {
   const grid = [];
-  // const rand = getRandomNumber(1, 10);
-  const rand = 1;
+//   const rand = getRandomNumber(0, 8);
+   let rand = 6;
   switch (rand) {
     case 0:
-      // Generate grid with random numbers between 0 and 100
+      // Left Side = large numbers ,,,,, Right Side = Small Numbers
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          const randomNumber = getRandomNumber(0, 100);
+          let randomNumber;
+          if(j < cols/2){
+          randomNumber = getRandomNumber(0, 100);
+          }else{
+            randomNumber = getRandomNumber(0, 10);
+          }
           row.push(randomNumber);
         }
         grid.push(row);
@@ -20,27 +25,36 @@ const gridNumbersPattern = (rows, cols) => {
       break;
 
     case 1:
-      // Generate grid with a certain pattern (example)
+      // Right side = larger numbers ,,,, Left side = small numbers
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+          let randomNumber;
+          if(j > cols/2){
+          randomNumber = getRandomNumber(0, 100);
+          }else{
+            randomNumber = getRandomNumber(0, 10);
+          }
+          row.push(randomNumber);
         }
         grid.push(row);
+
       }
       return grid;
       break;
 
     case 2:
-      // Generate grid with a certain pattern (example)
+      // Upper half = larger numbers ,,, lower half = small numbers
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+          let randomNumber;
+          if(i < rows/2){
+          randomNumber = getRandomNumber(0, 100);
+          }else{
+            randomNumber = getRandomNumber(0, 10);
+          }
+          row.push(randomNumber);
         }
         grid.push(row);
       }
@@ -48,13 +62,17 @@ const gridNumbersPattern = (rows, cols) => {
       break;
 
     case 3:
-      // Generate grid with a certain pattern (example)
+      // lower half = larger numbers ,,, upper half = small numbers
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+          let randomNumber;
+          if(i > cols/2){
+          randomNumber = getRandomNumber(0, 100);
+          }else{
+            randomNumber = getRandomNumber(0, 10);
+          }
+          row.push(randomNumber);
         }
         grid.push(row);
       }
@@ -62,13 +80,17 @@ const gridNumbersPattern = (rows, cols) => {
       break;
 
     case 4:
-      // Generate grid with a certain pattern (example)
+      // Odd or Even if sum of i and j
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+          let randomNumber;
+          if(i%2==0){
+          randomNumber = getRandomNumber(0, 100);
+          }else{
+            randomNumber = getRandomNumber(0, 10);
+          }
+          row.push(randomNumber);
         }
         grid.push(row);
       }
@@ -80,9 +102,13 @@ const gridNumbersPattern = (rows, cols) => {
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+          let randomNumber;
+          if(i == 0 || j == 0 || i == rows-1||j==cols-1){
+          randomNumber = getRandomNumber(0, 5);
+          }else{
+            randomNumber = getRandomNumber(0, 50);
+          }
+          row.push(randomNumber);
         }
         grid.push(row);
       }
@@ -90,17 +116,7 @@ const gridNumbersPattern = (rows, cols) => {
       break;
 
     case 6:
-      // Generate grid with a certain pattern (example)
-      for (let i = 0; i < rows; i++) {
-        const row = [];
-        for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
-        }
-        grid.push(row);
-      }
-      return grid;
+      return createSpiralGrid(rows,cols);
       break;
 
     case 7:
@@ -108,9 +124,9 @@ const gridNumbersPattern = (rows, cols) => {
       for (let i = 0; i < rows; i++) {
         const row = [];
         for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
+      // Determine the value based on the symmetry pattern
+      const value = i < rows / 2 ? i + 1 : rows - i;
+      row.push(value);
         }
         grid.push(row);
       }
@@ -123,35 +139,7 @@ const gridNumbersPattern = (rows, cols) => {
         const row = [];
         for (let j = 0; j < cols; j++) {
           // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
-        }
-        grid.push(row);
-      }
-      return grid;
-      break;
-
-    case 9:
-      // Generate grid with a certain pattern (example)
-      for (let i = 0; i < rows; i++) {
-        const row = [];
-        for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
-          row.push(number);
-        }
-        grid.push(row);
-      }
-      return grid;
-      break;
-
-    case 10:
-      // Generate grid with a certain pattern (example)
-      for (let i = 0; i < rows; i++) {
-        const row = [];
-        for (let j = 0; j < cols; j++) {
-          // Apply your desired pattern logic here
-          const number = (i + j) % 2 === 0 ? 1 : 0;
+          const number = getRandomNumber(0+i,i+j);
           row.push(number);
         }
         grid.push(row);
@@ -164,12 +152,57 @@ const gridNumbersPattern = (rows, cols) => {
   }
 };
 
-let grid = gridNumbersPattern(10, 10);
-console.log(grid);
-for (let i = 0; i < 10; i++) {
-  let s = "";
-  for (let j = 0; j < 10; j++) {
-    s += grid[i][j] + " ";
+
+
+
+
+function createSpiralGrid(rows, cols) {
+  const grid = [];
+
+  let value = 0;
+  let top = 0;
+  let bottom = rows - 1;
+  let left = 0;
+  let right = cols - 1;
+
+  while (top <= bottom && left <= right) {
+    if (!grid[top]) grid[top] = [];
+    if (!grid[bottom]) grid[bottom] = [];
+
+    // Fill top row
+    for (let i = left; i <= right; i++) {
+      grid[top][i] = value;
+      
+    }
+    top++;
+    value++;
+    // Fill right column
+    for (let i = top; i <= bottom; i++) {
+      if (!grid[i]) grid[i] = [];
+      grid[i][right] = value;
+    }
+    right--;
+    value++;
+
+
+    // Fill bottom row
+    for (let i = right; i >= left; i--) {
+      grid[bottom][i] = value;
+    }
+    bottom--;
+    value++;
+
+
+    // Fill left column
+    for (let i = bottom; i >= top; i--) {
+      grid[i][left] = value;
+    }
+    left++;
+    value++;
+
   }
-  console.log(s);
+
+  return grid;
 }
+
+export default gridNumbersPatternHelper
